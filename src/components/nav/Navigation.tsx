d030f967fd959b9f85d7fb649a9b30c61b26b9de
Marlena Broniewicz <bronka1.dev@gmail.com>
2025-09-12 17:00:55 +0200

    Smooth scrolling

--- src/components/nav/Navigation.tsx
+++ src/components/nav/Navigation.tsx
@@ -1,10 +1,32 @@
+"use client";
+
 import React from "react";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 import Link from "next/link";
 import Image from "next/image";
+import { useLenis } from "lenis/react";
+
 export const Navigation = () => {
+  const lenis = useLenis();
+
+  const scrollHandler = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    const href = e.currentTarget.getAttribute("href");
+    if (!href || !href.startsWith("#")) return;
+
+    const target = document.querySelector(href) as HTMLElement;
+    if (!target || !lenis) return;
+
+    lenis.scrollTo(target, {
+      offset: 0,
+      duration: 1.5,
+      easing: (t) => 1 - Math.pow(1 - t, 3),
+    });
+  };
+
   return (
-    <nav className="sticky flex justify-between items-center pt-3 z-10">
+    <nav className="fixed top-0 container flex justify-between items-center pt-3 z-10">
       <div>
         <Image
           src="/brovnie-logo.svg"
@@ -15,9 +37,21 @@ export const Navigation = () => {
       </div>
       <div className="flex gap-5">
         <ul className="flex gap-5 dark:text-white text-xl">
-          <Link href="/">Home</Link>
-          <Link href="/#about">About</Link>
-          <Link href="/#work">Work</Link>
+          <Link href="#home" legacyBehavior>
+            <a onClick={scrollHandler} className="dark:text-sky-50">
+              Home
+            </a>
+          </Link>
+          <Link href="#about" legacyBehavior>
+            <a onClick={scrollHandler} className="dark:text-sky-50">
+              About
+            </a>
+          </Link>
+          <Link href="#work" legacyBehavior>
+            <a onClick={scrollHandler} className="dark:text-sky-50">
+              Work
+            </a>
+          </Link>
         </ul>
         <ThemeSwitcher />
       </div>
