ac4501b3c0bd0d80c6773d5bc3a60449cf890526
Marlena Broniewicz <bronka1.dev@gmail.com>
2025-09-14 08:30:38 +0200

    Adjusted dark theme for skills

--- src/components/about/Skills.tsx
+++ src/components/about/Skills.tsx
@@ -6,80 +6,80 @@ const Skills = () => {
     <div>
       <div className="flex flex-wrap gap-3">
         <ul className="flex flex-wrap gap-3">
-          <li className="bg-green-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-green-100 dark:bg-green-800 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             React
           </li>
-          <li className="bg-green-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-green-100 dark:bg-green-800 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             ReactNative
           </li>
-          <li className="bg-green-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-green-100 dark:bg-green-800 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             VueJS
           </li>
-          <li className="bg-green-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-green-100 dark:bg-green-800 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             Angular
           </li>
         </ul>
         <ul className="flex flex-wrap gap-3">
-          <li className="bg-slate-200 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-slate-200 dark:bg-slate-600 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             CSS
           </li>
-          <li className="bg-slate-200 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-slate-200 dark:bg-slate-600 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             SASS
           </li>
-          <li className="bg-slate-200 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-slate-200 dark:bg-slate-600 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             TailwindCss
           </li>
-          <li className="bg-slate-200 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-slate-200 dark:bg-slate-600 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             Bootstrap
           </li>
         </ul>
         <ul className="flex flex-wrap gap-3">
-          <li className="bg-sky-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-sky-100 dark:bg-sky-800 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             NodeJS
           </li>
-          <li className="bg-sky-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-sky-100 dark:bg-sky-800 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             Laravel
           </li>
-          <li className="bg-sky-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-sky-100 dark:bg-sky-800 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             PHP
           </li>
         </ul>
         <ul className="flex flex-wrap gap-3">
-          <li className="bg-pink-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-pink-100 dark:bg-pink-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             MangoDB
           </li>
-          <li className="bg-pink-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-pink-100 dark:bg-pink-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             SQL
           </li>
-          <li className="bg-pink-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-pink-100 dark:bg-pink-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             PostSQL
           </li>
           <ul className="flex flex-wrap gap-3">
-            <li className="bg-indigo-100 rounded-md px-2 py-1 inline-block text-lg">
+            <li className="bg-indigo-100 dark:bg-indigo-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
               Wordpress
             </li>
-            <li className="bg-indigo-100 rounded-md px-2 py-1 inline-block text-lg">
+            <li className="bg-indigo-100 dark:bg-indigo-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
               Magento
             </li>
-            <li className="bg-indigo-100 rounded-md px-2 py-1 inline-block text-lg">
+            <li className="bg-indigo-100 dark:bg-indigo-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
               AWS
             </li>
-            <li className="bg-indigo-100 rounded-md px-2 py-1 inline-block text-lg">
+            <li className="bg-indigo-100 dark:bg-indigo-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
               Docker
             </li>
           </ul>
         </ul>
         <ul className="flex flex-wrap gap-3">
-          <li className="bg-fuchsia-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-fuchsia-100 dark:bg-fuchsia-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             Figma
           </li>
-          <li className="bg-fuchsia-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-fuchsia-100 dark:bg-fuchsia-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             AdomeXD
           </li>
-          <li className="bg-fuchsia-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-fuchsia-100 dark:bg-fuchsia-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             Illustrator
           </li>
-          <li className="bg-fuchsia-100 rounded-md px-2 py-1 inline-block text-lg">
+          <li className="bg-fuchsia-100 dark:bg-fuchsia-900 dark:text-white rounded-md px-2 py-1 inline-block text-lg">
             Affinity Design
           </li>
         </ul>
